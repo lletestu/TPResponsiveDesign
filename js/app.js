d9a697591c88c40bf1f65856be9f7604028c9b11
adejacques <alexdejacques@gmail.com>
2014-12-16 12:30:01 +0100

    final commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 // Code here
+$('.contentMetroList').each(function(){
+    var v2 = $(this).children();
+    var v3 = $(v2).children().length;
+    var v5 = $(v2).children('li:first').text();
+    var v6 = $(v2).children('li:last').text();
+    var v4 = $(this).parent();
+    var v7 = v4.parent();
+    v4.parent().children(".count").append( "<p> "+v5+" - " +v6+ "</p>" );
+    v4.parent().children(".station").append("<p> "+v3+" Stations</p>" );
+});
 
 
 /* Expand collapse on click */
-
 
 $(".showList").click(function () {
 
@@ -26,11 +35,11 @@ $(".search").on("keyup", function (e) {
         $(this).find('li').each(function(){
             // cache jquery var
             var current = $(this);
-            var text = current.text().toLowerCase();;
+            var text = current.text().toLowerCase();
             var article = container.closest("article");
             if (text.indexOf(input_value) > -1) {
                 //article.addClass("show");
-                article.removeClass("hidden")
+                article.removeClass("hidden");
                 i++;
                 return false;
 
@@ -38,7 +47,6 @@ $(".search").on("keyup", function (e) {
                 //article.hide();
                 article.addClass("hidden");
             }
-
         });
     });
 
